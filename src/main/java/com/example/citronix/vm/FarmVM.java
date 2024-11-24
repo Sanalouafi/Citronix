f4979b481a8c544a28698ee40dcf5c9040060285
Sanalouafi <sanalouafi2003@gmail.com>
2024-11-24 23:56:12 +0100

    add the FarmVM

--- src/main/java/com/example/citronix/vm/FarmVM.java
+++ src/main/java/com/example/citronix/vm/FarmVM.java
@@ -1,4 +1,17 @@
 package com.example.citronix.vm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FarmVM {
+
+    private Long id;
+    private String name;
+    private String location;
+    private double area;
+    private double totalRevenue;
 }
