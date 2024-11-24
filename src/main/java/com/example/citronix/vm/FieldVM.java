7fd8034ea5a99fac670ac8f43705d7096ce91e1f
Sanalouafi <sanalouafi2003@gmail.com>
2024-11-24 21:44:08 +0100

    add the fieldVM

--- src/main/java/com/example/citronix/vm/FieldVM.java
+++ src/main/java/com/example/citronix/vm/FieldVM.java
@@ -1,4 +1,17 @@
 package com.example.citronix.vm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FieldVM {
+
+    private Long id;
+    private Long farmId;
+    private Double area;
+    private Double maxAllowedArea;
+    private int totalFieldsInFarm;
 }
