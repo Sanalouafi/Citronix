4fc27788515c89772d944c83909de287756eeaaa
Sanalouafi <sanalouafi2003@gmail.com>
2024-11-24 23:33:26 +0100

    CIT-26,CIT-27,CIT-30,CIT-30,CIT-31,CIT-32,CIT-33,CIT-35,CIT-36,CIT-28 done

--- src/main/java/com/example/citronix/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/citronix/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -37,5 +42,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleSaleNotFoundException(SaleNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
-    
+    private ResponseEntity<Object> buildErrorResponse(Exception ex, HttpStatus status, WebRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", status.value());
+        errorDetails.put("error", status.getReasonPhrase());
+        errorDetails.put("message", ex.getMessage());
+        errorDetails.put("path", request.getDescription(false).replace("uri=", ""));
+
+        return new ResponseEntity<>(errorDetails, status);
+    }
 }
