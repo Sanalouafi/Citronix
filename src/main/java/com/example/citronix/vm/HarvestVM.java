c2e8390525943b0142a1665d50df0521f3228992
Sanalouafi <sanalouafi2003@gmail.com>
2024-11-24 23:53:39 +0100

    add the HarvestVM

--- src/main/java/com/example/citronix/vm/HarvestVM.java
+++ src/main/java/com/example/citronix/vm/HarvestVM.java
@@ -1,4 +1,20 @@
 package com.example.citronix.vm;
 
+import com.example.citronix.entity.enums.Season;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HarvestVM {
+
+    private Long id;
+    private Season season;
+    private LocalDate date;
+    private double totalQuantity;
+    private String status;
 }
